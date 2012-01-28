ee84b76587cae531e4ce7ff80333b83544ed38d8
Jeremias Volker <jeremias.volker@googlemail.com>
2012-01-28 11:23:25 +0100

    added KindleWriter class

---
+++ src/kindle/WriterRun.java
@@ -0,0 +1,16 @@
+package kindle;
+
+import processing.core.PApplet;
+
+public class WriterRun {
+	
+	private static KindleWriter kindleWriter;
+	
+	public static void main(String args[]) {
+		//PApplet.main(new String[] { readPcap.router66.WriterRun.class.getName() });
+		
+		kindleWriter = new KindleWriter();
+
+	}
+
+}
